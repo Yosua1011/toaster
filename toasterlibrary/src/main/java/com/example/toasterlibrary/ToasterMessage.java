9df8f338c188f4875c5f3618428133c9d9976d2c
Yosua Tresno Surojo <yosua1011@gmail.com>
2019-05-31 15:07:21 +0700

    Initial commit

---
+++ toasterlibrary/src/main/java/com/example/toasterlibrary/ToasterMessage.java
@@ -0,0 +1,11 @@
+package com.example.toasterlibrary;
+
+public class ToasterMessage {
+
+    public static void s(Context c,String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+
+}
